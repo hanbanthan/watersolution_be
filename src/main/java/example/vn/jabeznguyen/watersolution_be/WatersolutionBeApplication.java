0454591fbe376a1cb90905d0bd0ad428b953c889
giapnguyen <giapnguyen2004@gmail.com>
2025-11-15 19:16:59 +0700

    init

---
+++ src/main/java/example/vn/jabeznguyen/watersolution_be/WatersolutionBeApplication.java
@@ -0,0 +1,13 @@
+package example.vn.jabeznguyen.watersolution_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatersolutionBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatersolutionBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/vn/jabeznguyen/watersolution_be/WatersolutionBeApplicationTests.java
@@ -0,0 +1,13 @@
+package example.vn.jabeznguyen.watersolution_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatersolutionBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
